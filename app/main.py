3bd0621807d1361079d2873b76d6c3a2228567d4
mukul-457 <mukulacharya.1911@gmail.com>
2024-08-08 23:24:43 +0530

    pass 1st stage

--- app/main.py
+++ app/main.py
@@ -2,8 +2,8 @@ import sys
 
 
 def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    print("Logs from your program will appear here!", file=sys.stderr)
+    # # You can use print statements as follows for debugging, they'll be visible when running tests.
+    # print("Logs from your program will appear here!", file=sys.stderr)
 
     if len(sys.argv) < 3:
         print("Usage: ./your_program.sh tokenize <filename>", file=sys.stderr)
@@ -20,10 +20,10 @@ def main():
         file_contents = file.read()
 
     # Uncomment this block to pass the first stage
-    # if file_contents:
-    #     raise NotImplementedError("Scanner not implemented")
-    # else:
-    #     print("EOF  null") # Placeholder, remove this line when implementing the scanner
+    if file_contents:
+        raise NotImplementedError("Scanner not implemented")
+    else:
+        print("EOF  null") # Placeholder, remove this line when implementing the scanner
 
 
 if __name__ == "__main__":
