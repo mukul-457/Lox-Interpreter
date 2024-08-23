f2b12e707fac12760fb34be6a9611099a1ecc624
mukul-457 <mukulacharya.1911@gmail.com>
2024-08-24 03:16:40 +0530

    Assigned file reading to Scanner obj and reading file line by line instead of all at once . Addded support for divison and comment

--- app/main.py
+++ app/main.py
@@ -8,34 +8,43 @@ class Scanner():
                                     ".": "DOT", "," : "COMMA", "+": "PLUS",
                                     "*": "STAR", "-": "MINUS", ";": "SEMICOLON",
                                     "=" : "EQUAL", "!" : "BANG" ,"<" : "LESS",
-                                    ">":"GREATER"}
+                                    ">":"GREATER", "/" : "SLASH"}
         self.bi_character_token = {"==" : "EQUAL_EQUAL","!=" :"BANG_EQUAL",
                                    "<=" : "LESS_EQUAL",
-                                   ">=":"GREATER_EQUAL"}
+                                   ">=":"GREATER_EQUAL", "//": "COMMENT"}
         
         self.invalid_tokens = set(["$", "#", "@", "%"])
+
+    def scan_file(self,filepath):
+        rt = 0
+        line_number = 1
+        with open(filepath) as file:
+            for line in file:
+                rt = rt or self.scan_content(line, line_number)
+                line_number+=1
+        print("EOF", "" ,"null")
+        return rt
     
-    def scan_content(self, content):
+    def scan_content(self, content,line_number):
         exit_code = 0
         n =  len(content)
         i = 0
-        line_number = 1
         while (i < n):
-            if content[i] == "\n":
-                line_number +=1
-                i+=1
-            elif content[i] in  self.invalid_tokens:
+            if content[i] in  self.invalid_tokens:
                 print(f"[line {line_number}] Error: Unexpected character: {content[i]}", file= sys.stderr)
                 exit_code = 65
                 i+=1
             elif i+1 < n and content[i]+content[i+1] in self.bi_character_token:
                 ele = content[i] + content[i+1]
+                if(ele == "//"):
+                    break
                 print(self.bi_character_token[ele]  , ele , "null")
                 i+=2
             elif content[i] in self.uni_character_token:
                 print(self.uni_character_token[content[i]] , content[i] , "null")
                 i+=1
-        print("EOF" ,"", "null") 
+            else:
+                i+=1
         return exit_code
 
 def main():
@@ -53,58 +62,9 @@ def main():
         print(f"Unknown command: {command}", file=sys.stderr)
         exit(1)
 
-    with open(filename) as file:
-        file_contents = file.read()
-
-    return_code = Scanner().scan_content(file_contents)
-
-    exit(return_code)
-
-
-def scan_for_tokens(content):
-    return_code= 0
-    tokens = {"(" : "LEFT_PAREN", ")": "RIGHT_PAREN", 
-              "{" : "LEFT_BRACE", "}" : "RIGHT_BRACE",
-              ".": "DOT", "," : "COMMA", "+": "PLUS",
-              "*": "STAR", "-": "MINUS", ";": "SEMICOLON"}
-    invalid_tokens = set(["$", "#", "@", "%"])
-    line_number  = 1
-    equals = 0
-    bangs= 0    
-    for chr in  content:
-        if equals and chr != "=":
-            print("EQUAL = null")
-            equals= 0
-        if bangs and chr != "=":
-            print("BANG ! null")
-            bangs = 0
-        if chr in tokens:
-            print(tokens[chr], chr , "null")
-        elif chr in invalid_tokens:
-            print(f"[line {line_number}] Error: Unexpected character: {chr}", file= sys.stderr)
-            return_code = 65
-        elif chr == "\n":
-            line_number += 1
-        elif chr == "=":
-            if equals:
-                print("EQUAL_EQUAL == null")
-                equals = 0
-            elif bangs:
-                print("BANG_EQUAL != null")
-                bangs = 0
-            else:
-                equals += 1
-        elif chr == "!":
-            bangs += 1
-
-    if equals:
-        print("EQUAL = null")
-    if bangs:
-        print("BANG ! null")
-
-    return return_code
-        
-    
+    scaner = Scanner()
+    exit_code = scaner.scan_file(filename)
+    exit(exit_code)    
 
 if __name__ == "__main__":
     main()
